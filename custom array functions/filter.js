073695f6a1bd2090521c6c116d44298b1b166d89
ZAAC <razaraizaroon@gmail.com>
2026-05-25 16:15:31 +0500

    check working

--- custom array functions/filter.js
+++ custom array functions/filter.js
@@ -9,3 +9,8 @@ Array.prototype.myfilter = function(cb){
     return temp;
 }
 
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+let evennumbers = numbers.myfilter(number => number %2 === 0);
+
+console.log(evennumbers);
