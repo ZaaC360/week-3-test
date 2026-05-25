db77dd076db8b8a7c9e29d7d59cf65665281f24f
ZAAC <razaraizaroon@gmail.com>
2026-05-25 16:00:52 +0500

    implemented map fn

---
+++ custom map/map.js
@@ -0,0 +1,11 @@
+
+Array.prototype.myMap = function(cb) {
+    let result = [];
+
+    for(let i = 0; i < this.length; i++) {
+        result.push(cb(this[i], i, this));
+    }
+
+    return result;
+}
+
